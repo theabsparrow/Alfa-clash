9464a145ea8f62e94925606e69ce84a4f9709a91
theabsparrow <absbashar04@gmail.com>
2024-03-08 01:17:30 +0600

    score and life section

--- script.js
+++ script.js
@@ -8,10 +8,20 @@ function playGround () {
     const playGround = document.getElementById("play-ground");
     playGround.classList.remove("hidden")
 }
+
+
+
 function setColorOfAlphabet (alphabetId){
 const theAlphabet = document.getElementById(alphabetId);
 theAlphabet.classList.add("bg-[#FFA500]");
 }
+
+function removeColorOfAlphabet (alphabetId){
+    const theAlphabet = document.getElementById(alphabetId);
+    theAlphabet.classList.remove("bg-[#FFA500]");
+    }
+
+
 
 function contineuTheGame () {
     const alphabetCollection = "abcdefghijklmnopqrstuvwxyz"
@@ -22,7 +32,35 @@ function contineuTheGame () {
    const desireAlphabet = document.getElementById("desire-alphabet");
    desireAlphabet.innerText = alphabet;
    setColorOfAlphabet(alphabet)
+   
+  
 }
+
+function userpressedkey (event){
+    const pressedKey = event.key;
+    const desireAlphabet = document.getElementById("desire-alphabet");
+    const showingKey = desireAlphabet.innerText;
+    const desireKey = showingKey.toLowerCase()
+    if(pressedKey === desireKey){
+        contineuTheGame()
+        removeColorOfAlphabet(desireKey)
+        const defaultValue = document.getElementById("default-value")
+        const defaultScore = defaultValue.innerText;
+        const defaultScoreNum = parseInt(defaultScore);
+        const setNewScore = defaultScoreNum + 1;
+        defaultValue.innerText = setNewScore;
+        
+    }
+    else{
+       const initialLife = document.getElementById("initial-life");
+       const initialLifeText = initialLife.innerText;
+       const initialLifeNum = parseInt(initialLifeText);
+       const reduceInitialLIfe = initialLifeNum - 1;
+       initialLife.innerText = reduceInitialLIfe;
+    }
+}
+document.addEventListener ("keyup" , userpressedkey)
+
 
 
 function playGame (){
